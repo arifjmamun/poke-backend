2547b774a6634937f66f75636f578aa0c007d7e3
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-10-02 23:19:19 +0600

    Updated environment config

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,7 +3,11 @@ import { cleanEnv, port, str } from 'envalid';
 const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
-    PORT: port(),
+    SERVER_PORT: port(),
+    CLIENT_ORIGIN_URL: str(),
+    AUTH0_AUDIENCE: str(),
+    AUTH0_DOMAIN: str(),
+    POKEMON_BASE_URL: str(),
   });
 };
 
